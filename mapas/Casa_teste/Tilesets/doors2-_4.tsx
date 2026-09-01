64daf96df7ea0e538f05081c86201604aa46d18a
diego <diego.dos248@gmail.com>
2026-09-01 17:34:01 -0300

    Casa_tese

---
+++ mapas/Casa_teste/Tilesets/doors2-_4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="doors2-_4" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../../../../tiles/Epic RPG World - Village(interiors) V1.3/Epic RPG World - Village(interiors) V1.3/assets/windows_and_doors_sprites/no_sunlight/doors2-_4.png" width="64" height="96"/>
+</tileset>
